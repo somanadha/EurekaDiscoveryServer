68a76e678743fa41af7e4430e693b4a0dc3bf9b8
Somanadha Satya Dev Bulusu <soma_satya@yahoo.com>
2024-06-25 20:02:59 +0530

    Initial Commit

---
+++ src/main/java/com/bst/eds/EurekaDiscoveryServerApplication.java
@@ -0,0 +1,16 @@
+package com.bst.eds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryServerApplication.class, args);
+		System.out.println("EurekaDiscoveryServer is running...");
+	}
+
+}
